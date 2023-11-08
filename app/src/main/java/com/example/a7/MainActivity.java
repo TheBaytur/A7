eb5f726abf0849d29ea4f4045088eea12708a02f
TheBaytur <bayturcodding@gmail.com>
2023-11-09 01:36:34 +0800

    Text redactor

--- app/src/main/java/com/example/a7/MainActivity.java
+++ app/src/main/java/com/example/a7/MainActivity.java
@@ -3,12 +3,67 @@ package com.example.a7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText user_name_field, user_bio_field;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user_name_field = findViewById(R.id.user_name_field);
+        user_bio_field = findViewById(R.id.user_bio_field);
+    }
+
+    public void saveData(View view){
+        String user_name = user_name_field.getText().toString();
+        String user_bio = user_bio_field.getText().toString();
+
+        try {
+            FileOutputStream fileOutput = openFileOutput("user_data.txt", MODE_PRIVATE);
+            fileOutput.write((user_name + ". " + user_bio).getBytes());
+            fileOutput.close();
+
+            user_name_field.setText("");
+            user_bio_field.setText("");
+            Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Can't identify file", Toast.LENGTH_SHORT).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getData(View view){
+        try {
+            FileInputStream fileInput = openFileInput("user_data.txt");
+            InputStreamReader reader = new InputStreamReader(fileInput);
+            BufferedReader bR = new BufferedReader(reader);
+
+            StringBuilder stringBuffer = new StringBuilder();
+            String lines = "";
+            while((lines = bR.readLine()) !=null){
+                stringBuffer.append(lines).append("\n");
+            }
+
+            Toast.makeText(this, stringBuffer, Toast.LENGTH_LONG).show();
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
